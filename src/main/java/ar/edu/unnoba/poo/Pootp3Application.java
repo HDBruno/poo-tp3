fa4a8a8837d5746edf0782c3d3d2fe01fedd6e5a
Herni <hernan.bruno@outlook.com>
2021-11-30 22:58:23 -0300

    First commit

---
+++ src/main/java/ar/edu/unnoba/poo/Pootp3Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pootp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pootp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unnoba/poo/Pootp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pootp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
